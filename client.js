2328f2f4b29d60591db216c0797f4711bd091c31
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-10-04 23:33:44 -0500

    add input from user to table on DOM

--- client.js
+++ client.js
@@ -2,9 +2,9 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jQ working');
-    
+    $('#addInfo').on('click', getEmployeeInfo);
     $('#addInfo').on('click', inputClear);
-    
+    $('')
 }
 
 function inputClear() {
@@ -13,4 +13,17 @@ function inputClear() {
     $("#employeeID").val("");
     $("#employeeTitle").val("");
     $("#employeeSalary").val("");
+  }
+
+  function getEmployeeInfo(){
+      console.log('in getEmployeeInfo');
+      $('#employeeInfo').append('<tr>' + 
+            '<td>' + $('#employeeFirstName').val() + '</td>' +
+            '<td>' + $('#employeeLastName').val() + '</td>' +
+            '<td>' + $('#employeeID').val() + '</td>' +
+            '<td>' + $('#employeeTitle').val() + '</td>' +
+            '<td>' + $('#employeeSalary').val() + '</td>' +
+            '<td><button class="deleteEmployee">Delete</button></td>' +
+       '</tr>');
+     
   }
