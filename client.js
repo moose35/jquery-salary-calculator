629de3d374a33b4cfc6e2734ff4f37c8dd9f6baf
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-10-05 12:13:54 -0500

    delete table rows populated from user input

--- client.js
+++ client.js
@@ -2,11 +2,12 @@ $(document).ready(onReady);
 
 let annualSalary = [];
 
-function onReady(){
+function onReady() {
     console.log('jQ working');
     $('#addInfo').on('click', getEmployeeInfo);
     $('#addInfo').on('click', inputClear);
-   // $('#addInfo').on('click', storeSalary);
+    $('tbody').on('click', '.deleteEmployee', handleDelete);
+    // $('#addInfo').on('click', storeSalary);
 }
 
 function inputClear() {
@@ -15,22 +16,26 @@ function inputClear() {
     $("#employeeID").val("");
     $("#employeeTitle").val("");
     $("#employeeSalary").val("");
-  }
+}
 
-  function getEmployeeInfo(){
-      console.log('in getEmployeeInfo');
-      $('#employeeInfo').append('<tr>' + 
-            '<td>' + $('#employeeFirstName').val() + '</td>' +
-            '<td>' + $('#employeeLastName').val() + '</td>' +
-            '<td>' + $('#employeeID').val() + '</td>' +
-            '<td>' + $('#employeeTitle').val() + '</td>' +
-            '<td>' + $('#employeeSalary').val() + '</td>' +
-            '<td><button class="deleteEmployee">Delete</button></td>' +
-       '</tr>');
-     
-  }
+function getEmployeeInfo() {
+    console.log('in getEmployeeInfo');
+    $('#employeeInfo').append('<tr>' +
+        '<td>' + $('#employeeFirstName').val() + '</td>' +
+        '<td>' + $('#employeeLastName').val() + '</td>' +
+        '<td>' + $('#employeeID').val() + '</td>' +
+        '<td>' + $('#employeeTitle').val() + '</td>' +
+        '<td>' + $('#employeeSalary').val() + '</td>' +
+        '<td><button class="deleteEmployee">Delete</button></td>' +
+        '</tr>');
+}
 
-  /*function storeSalary(){
-      $('#employeeSalary').add(annualSalary);
-      console.log('in storeSalary');
-  }*/
+function handleDelete(){
+    $('.deleteEmployee').on('click', 
+    console.log('Delete clicked'));
+    $(this).closest('tr').remove();
+  }
+/*function storeSalary(){
+    $('#employeeSalary').add(annualSalary);
+    console.log('in storeSalary');
+}*/
